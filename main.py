186bf1679570132be1ce56893c16b3dbe1ff514e
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-04-10 21:09:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,21 @@
 # print(fibo(10))
 # print(fibo(15))
 
-# Дргуе завдання
+# Друге завдання
 
-import re
-from typing import Callable, Generator
+# import re
+# from typing import Callable, Generator
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+# text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
-def generator_numbers(text: str) -> Generator[float]:
-    for num in re.findall(r'\d+(?:\.\d+)', text):
-        yield float(num)
+# def generator_numbers(text: str) -> Generator[float, None, None]:
+#     for num in re.findall(r"\d+(?:\.\d+)", text):
+#         yield float(num)
 
-def sum_profit(text: str, func: Callable[[str], Generator[float]]) -> float:
-    return sum(func(text))
+# def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+#     return sum(func(text))
 
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+# total_income = sum_profit(text, generator_numbers)
+# print(f"Загальний дохід: {total_income}")
+
+# Третє завдання
